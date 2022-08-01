ee246576b61c80b65c9fc2e1bcc154e9e14b9753
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2022-08-01 15:27:57 +0900

    feat: Add file-read.py

---
+++ file-read.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+## -*- coding: utf-8 -*- vim:shiftwidth=4:expandtab:
+
+import sys
+## Python 2
+#import errno
+
+
+try:
+    with open("msg.txt") as f:
+        for line in f:
+            line = line.strip()
+            if line == '' or line.startswith('#'):
+                continue
+            print(line)
+except FileNotFoundError as e:
+    print('%s: %s' % (__file__, e), file=sys.stderr)
+## Python 2
+#except IOError as e:
+#    if e.errno == errno.ENOENT:
+#        print >>sys.stderr, '%s: %s' % (__file__, e)
+#    else:
+#        raise
